c4220f880652bbfd55b33610c595a7ea322dc5d0
jeffyactive <jeff@reelyactive.com>
2016-08-06 13:52:12 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,50 @@
+/*
+ * This Sniffypedia is made available under the Open Database License:
+ * http://opendatacommons.org/licenses/odbl/1.0/.
+ * Any rights in individual contents of the database are licensed under
+ * Creative Commons Attribution-ShareAlike 4.0 International:
+ * https://creativecommons.org/licenses/by-sa/4.0/
+ */
+
+
+// Bluetooth Low Energy 16-bit UUIDs
+// See: https://www.bluetooth.com/specifications/assigned-numbers/16-bit-uuids-for-members
+var ble_uuid16 = {
+
+};
+
+
+// Bluetooth Low Energy 128-bit UUIDs
+var ble_uuid128 = {
+  "7265656c794163746976652055554944": "Product/reelyActive_RA-R436"
+};
+
+
+// Bluetooth Low Energy company identifiers
+// See:https://www.bluetooth.com/specifications/assigned-numbers/company-identifiers
+var ble_companyIdentifiers = {
+
+};
+
+
+// Bluetooth Low Energy iBeacons (128-bit UUIDs)
+var ble_iBeacons = {
+
+};
+
+
+// Bluetooth Low Energy
+var ble = {
+  uuid16: ble_uuid16,
+  uuid128: ble_uuid128,
+  companyIdentifiers: ble_companyIdentifiers,
+  iBeacons: ble_iBeacons
+};
+
+
+// The Sniffypedia index
+var sniffypedia_index = {
+  ble: ble
+};
+
+
---
+++ js/sniffypedia.js
@@ -0,0 +1,33 @@
+/*
+ * This Sniffypedia is made available under the Open Database License:
+ * http://opendatacommons.org/licenses/odbl/1.0/.
+ * Any rights in individual contents of the database are licensed under
+ * Creative Commons Attribution-ShareAlike 4.0 International:
+ * https://creativecommons.org/licenses/by-sa/4.0/
+ */
+
+
+/**
+ * sniffypedia Module
+ * All of the JavaScript specific to the Sniffypedia is contained inside this
+ * angular module.  The only external dependencies are:
+ * - AngularUI Bootstrap
+ * - cormorant (reelyActive)
+ * - cuttlefish (reelyActive)
+ */
+angular.module('sniffypedia', [ 'ui.bootstrap' ])
+
+
+/**
+ * InteractionCtrl Controller
+ * Handles the manipulation of all variables accessed by the HTML view.
+ */
+.controller('InteractionCtrl', function($scope) {
+
+  // Variables accessible in the HTML scope
+  $scope.index = sniffypedia_index;
+  $scope.protocol = 'Select protocol';
+  $scope.identifier = 'Select identifier';
+  $scope.value = 'Select value';
+
+});
