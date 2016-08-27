30f43884d6423bb975ed028c47a49b76b2cb4c31
jeffyactive <jeff@reelyactive.com>
2016-08-26 22:55:22 -0400

    Added Kontakt.io Beacon

--- js/index.js
+++ js/index.js
@@ -31,7 +31,8 @@ var ble_companyIdentifiers = {
 
 // Bluetooth Low Energy iBeacons (128-bit UUIDs)
 var ble_iBeacons = {
-  "b9407f30f5f8466eaff925556b57fe6d": "Product/Estimote_Beacon"
+  "b9407f30f5f8466eaff925556b57fe6d": "Product/Estimote_Beacon",
+  "f7826da64fa24e988024bc5b71e0893e": "Product/Kontakt_Beacon"
 };
 
 
