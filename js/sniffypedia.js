6ddce654fa3478011727d15a33bacce3fe83a16e
jeffyactive <jeff@reelyactive.com>
2016-08-27 09:59:10 -0400

    Implemented bubble update functionality

--- js/sniffypedia.js
+++ js/sniffypedia.js
@@ -7,6 +7,21 @@
  */
 
 
+SNIFFYPEDIA_STORY = {
+  "@context": {
+    "schema": "http://schema.org/"
+  },
+  "@graph": [
+    {
+      "@id": "product",
+      "@type": "schema:Product",
+      "schema:name": "Sniffypedia",
+      "schema:image": "http://sniffypedia.org/images/working-logo.png"
+    }
+  ]
+};
+
+
 /**
  * sniffypedia Module
  * All of the JavaScript specific to the Sniffypedia is contained inside this
@@ -23,26 +38,20 @@ angular.module('sniffypedia', [ 'ui.bootstrap', 'reelyactive.cormorant',
  * InteractionCtrl Controller
  * Handles the manipulation of all variables accessed by the HTML view.
  */
-.controller('InteractionCtrl', function($scope) {
+.controller('InteractionCtrl', function($scope, cormorant) {
 
   // Variables accessible in the HTML scope
   $scope.index = sniffypedia_index;
   $scope.protocol = 'Select protocol';
   $scope.identifier = 'Select identifier';
   $scope.value = 'Select value';
+  $scope.story = SNIFFYPEDIA_STORY;
 
-  $scope.story = {
-    "@context": {
-      "schema": "http://schema.org/"
-    },
-    "@graph": [
-      {
-        "@id": "product",
-        "@type": "schema:Product",
-        "schema:name": "Sniffypedia",
-        "schema:image": "http://sniffypedia.org/images/working-logo.png"
-      }
-    ]
-  };
+  $scope.updateStory = function(url) {
+    delete $scope.story;
+    cormorant.getStory(url, function(story, url) {
+      $scope.story = story;
+    });
+  }
 
 });
