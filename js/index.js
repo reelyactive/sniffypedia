0df0d578a77f7e01dbf05434fed01f9d321ada06
jeffyactive <jeff@reelyactive.com>
2016-08-27 16:23:03 -0400

    Added Orange Beacon

--- js/index.js
+++ js/index.js
@@ -38,6 +38,7 @@ var ble_companyIdentifiers = {
 // Bluetooth Low Energy iBeacons (128-bit UUIDs)
 var ble_iBeacons = {
   "2f234454cf6d4a0fadf2f4911ba9ffa6": "Product/Radius-Networks_Beacon",
+  "3d4f13b4d1fd404980e5d3edcc840b69": "Product/Orange_Beacon",
   "8deefbb9f7384297804096668bb44281": "Product/Roximity_Beacon",
   "b9407f30f5f8466eaff925556b57fe6d": "Product/Estimote_Beacon",
   "f7826da64fa24e988024bc5b71e0893e": "Product/Kontakt_Beacon"
