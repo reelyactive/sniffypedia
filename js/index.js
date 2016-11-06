363e123d2217d1d52ee127937907af070c75e306
jeffyactive <jeff@reelyactive.com>
2016-11-05 22:17:39 -0400

    Added Jawbone

--- js/index.js
+++ js/index.js
@@ -29,6 +29,8 @@ var ble_uuid16 = {
   "feaa": "Product/Google_Eddystone/",
   "fee7": "Organization/Tencent_Holdings_Limited/",
   "fee6": "Organization/Seed_Labs_Inc/",
+  "fedd": "Organization/Jawbone/",
+  "fedc": "Organization/Jawbone/",
   "feba": "Organization/Tencent_Holdings_Limited/",
   "fea0": "Organization/Google/",
   "fe9f": "Organization/Google/",
