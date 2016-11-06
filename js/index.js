3ee2ca4e02e9e5abcb615ebc4d7199654bd0ee38
jeffyactive <jeff@reelyactive.com>
2016-11-05 20:25:20 -0400

    Added Gimbal member UUIDs

--- js/index.js
+++ js/index.js
@@ -10,6 +10,8 @@
 // Bluetooth Low Energy 16-bit UUIDs
 // See: https://www.bluetooth.com/specifications/assigned-numbers/16-bit-uuids-for-members
 var ble_uuid16 = {
+  "fefd": "Organization/Gimbal_Inc/",
+  "fefc": "Organization/Gimbal_Inc/",
   "fefa": "Organization/PayPal_Inc/",
   "fef9": "Organization/PayPal_Inc/",
   "fef4": "Organization/Google/",
